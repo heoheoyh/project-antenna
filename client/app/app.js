397259e6a047dc9ef457225d637d483e0e52034c
Yunhee-Heo <heoheoyh@gmail.com>
2016-02-06 15:36:32 +0900

    apply angular metarial module

--- client/app/app.js
+++ client/app/app.js
@@ -10,7 +10,8 @@ angular.module('projectHeoApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'validation.match'
+  'validation.match',
+  'ngMaterial'
 ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider
